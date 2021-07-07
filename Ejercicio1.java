b31fe3717c6228b37c17e37abcc5259a67693747
RaulSE98 <56968210+RaulSE98@users.noreply.github.com>
2021-07-07 12:48:33 +0200

    Create Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,21 @@
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 2;
+		int n2 = 4;
+		
+		System.out.println("El primer valor numerico es de " + n1);
+		System.out.println("El segundo valor numerico es de " + n2);
+		if (n1 > n2) {
+			System.out.println("El primer valor numerico es mayor al segundo");
+		} else if (n1 == n2) {
+			System.out.println("El primer y segundo valor numericos son iguales");
+		}else {
+			System.out.println("El segundo valor numerico es mayor al primero");
+		}
+
+	}
+
+}
